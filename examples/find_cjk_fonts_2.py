217c9b3dd48312bbc7f6bb8c64cc0261c70abe76
Nattōsai Mitō <flow4re2c@gmail.com>
2021-09-09 21:58:00 +0900

    modify one of examples

--- examples/find_cjk_fonts_2.py
+++ examples/find_cjk_fonts_2.py
@@ -1,11 +1,5 @@
-from kivyx.utils.fontfinder import (
-    enum_fonts_from_text, LANG_TEXT_MAP as M,
-)
+from kivyx.utils.fontfinder import enum_fonts_from_text
 
 print(f"\n-- list of fonts that would support all the CJK glyphs --")
-for font in sorted(
-        enum_fonts_from_text(''.join((
-            M['ko'], M['zh-Hant'], M['zh-Hans'], M['ja'],
-        ))),
-        key=lambda font: font.name):
+for font in sorted(enum_fonts_from_text('안經经経녕傳传伝'), key=lambda font: font.name):
     print(font.name)
